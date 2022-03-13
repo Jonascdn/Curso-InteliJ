0e721512072291be08d79edd396d8eccb285a48c
Jonas <jonasklgw@gmail.com>
2022-03-12 22:15:16 -0300

    Cauculadora c java

--- src/br/com/dio/Prim.java
+++ src/br/com/dio/Prim.java
@@ -5,11 +5,48 @@ import br.com.dio.model.Ramster;
 public class Prim {
     public static void main(String[] args) {
         Ramster ramster = new Ramster();
-
         System.out.println(ramster);
+
+
+        Livro Livro = new Livro("O problema dos 3 corpos",  300);
 
         //        int a = 9;
 //        int b = 10;
 //        System.out.println("Olá mundo!" + (a+b));
+    }
+}
+
+
+class Livro {
+    private String nome;
+    private Integer numPaginas;
+
+    public Livro(String nome, Integer numPaginas) {
+        this.nome = nome;
+        this.numPaginas = numPaginas;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getNumPaginas() {
+        return numPaginas;
+    }
+
+    public void setNumPaginas(Integer numPaginas) {
+        this.numPaginas = numPaginas;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "nome='" + nome + '\'' +
+                ", numPaginas=" + numPaginas +
+                '}';
     }
 }
---
+++ src/br/com/dio/cauculadora/Cauculadora.java
@@ -0,0 +1,46 @@
+package br.com.dio.cauculadora;
+
+import java.util.Scanner;
+
+public class Cauculadora {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+
+        int a, b;
+
+        System.out.println("Digite o primeiro valor");
+        a = scan.nextInt();
+        System.out.println("Digite o segundo valor");
+        b = scan.nextInt();
+
+        int somar = somar(a, b);
+        int subtrair = subtrair(a, b);
+        int multiplicar = multiplicar(a, b);
+        float dividir = dividir(a, b);
+
+
+        System.out.println("Somar: " + somar);
+        System.out.println("subt: " + subtrair);
+        System.out.println("mult: " + multiplicar);
+        System.out.println("Divi: " + dividir);
+
+
+
+    }
+
+    public static int somar(int a, int b) {
+        return a + b;
+    }
+    public static int subtrair(int a, int b) {
+        return a - b;
+    }
+    public static float dividir(float a, float b) {
+        return a / b;
+    }
+    public static int multiplicar(int a, int b) {
+        return a * b;
+    }
+
+
+}
